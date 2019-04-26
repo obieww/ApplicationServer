bb786d9fcc58a256b26dc82dcd7fc0738c980983
Hugh <deng.ch@northeastern.edu>
2019-04-25 22:16:50 -0700

    add env to run on multiple servers

--- src/main/java/com/obiew/ApplicationServerApplication.java
+++ src/main/java/com/obiew/ApplicationServerApplication.java
@@ -6,10 +6,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.obiew.Controllers", "com.obiew.Entities", "com.obiew.Repositories"})
 @EnableSwagger2
-public class LoadBalanceApplication {
+public class ApplicationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoadBalanceApplication.class, args);
+        SpringApplication.run(ApplicationServerApplication.class, args);
     }
 
 }
